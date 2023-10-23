b8c648e662e51b3cb600cf76a0c28426b8400a17
Topaz712 <tzam712@gmail.com>
2023-10-22 19:14:55 -0500

    (home.component.ts) map and filter operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, interval, Observable } from 'rxjs';
 import { timeout } from 'rxjs-compat/operator/timeout';
+import { map, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
@@ -21,7 +22,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(handler: () => {
         observer.next(count);
-        if (count === 2) {
+        if (count === 5) {
           observer.complete();
         }
         if (count > 3) {
@@ -31,7 +32,12 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, timeout: 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+
+    this.firstObsSubscription =     customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
